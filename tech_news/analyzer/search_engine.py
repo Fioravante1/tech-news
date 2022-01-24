ac4c29d678717c93aa995122006fb1b11a62b75b
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-01-24 18:46:20 -0300

    feat: add funcao search by title - requisito seis

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import find_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    result = find_news()
+    list_title_url = []
+    for notice in result:
+        if notice["title"].lower() == title.lower():
+            list_title_url.append((notice["title"], notice["url"]))
+    return list_title_url
 
 
 # Requisito 7
