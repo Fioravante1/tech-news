06752d666c8072b73c2bf92441e00a33b6184c67
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-01-29 14:12:49 -0300

    feat: add funcao top 5 news - requisito dez

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,23 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
+# Consultei repositorio de Vitor cabrera
+# assim que tive melhor entendimento do sort
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result = find_news()
+    list_title_url = []
+
+    result.sort(
+        key=lambda x: x["shares_count"] + x["comments_count"], reverse=True
+    )
+
+    cinco_mais = result[:5]
+
+    for notice in cinco_mais:
+        list_title_url.append((notice["title"], notice["url"]))
+
+    return list_title_url
 
 
 # Requisito 11
